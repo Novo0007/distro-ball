026b6956616f642b176159b8a3167533fde71cf5
NNCW2 <106741077+Novo0007@users.noreply.github.com>
2024-08-20 04:58:20 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,19 +15,19 @@ bubble.createBoard()
 bubble.load_bubble()
 let mySprite = sprites.create(img`
     f f f f f f f f f f f f f f f f 
-    f f f f f f 7 f f 7 7 f f f f f 
-    f 7 7 7 7 7 7 7 7 7 f 7 7 7 f f 
-    f 7 f f f f f f f f f f f f f f 
-    f 7 f 7 7 f f 7 f 7 7 f f 7 f f 
-    f 7 f 7 f 7 f 7 f 7 f 7 f 7 f f 
-    f 7 f 7 f 7 f 7 f 7 f 7 f 7 f f 
-    f 7 f 7 f 7 f 7 f 7 f 7 f 7 f f 
-    f 7 f 7 f 7 7 f f 7 f 7 7 f f f 
-    f 7 f f f f f f f f f f f f f f 
-    f 7 7 7 7 7 7 7 7 7 7 f f f f f 
-    f f f f f f f f f f f f f 7 f f 
-    f f f f f f 7 7 f f 7 f 7 7 f f 
-    . f f f 7 7 7 f f 7 7 7 7 f f . 
-    . f f 7 7 7 f f f 7 f 7 f f f . 
-    . . . f f f f f f f f f f . . . 
+    f f f f f 5 5 5 5 5 f f f f f f 
+    f f f 5 5 f f f f 5 5 5 f f f f 
+    f f 5 5 f f f f 5 f f 5 f f f f 
+    f f 5 f 5 f f f 5 f f 5 f f f f 
+    f 5 5 f 5 5 f 5 f f f 5 f f f f 
+    f 5 f f 5 5 5 5 f f f f f f f f 
+    f 5 f 5 5 f 5 5 f f f f f f f f 
+    f 5 f 5 f f f 5 f f f f f f f f 
+    f 5 f 5 f f f 5 f f f f f f f f 
+    f 5 f 5 f f f f f f f f f f f f 
+    f f 5 f f f f f f f f f f f f f 
+    f f 5 5 5 f f f f f 5 f f f f f 
+    f f f f 5 5 5 5 5 5 5 f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
     `, SpriteKind.Player)
